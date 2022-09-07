0f0824dd79a4a82394c7d380ab64a9f26704ba56
devcokhi <69025946+devcokhi@users.noreply.github.com>
2022-09-07 16:49:34 +0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,9 @@ export default function Footer() {
     <>
       <footer className={styles.footer}>
         ALO NHÀ TRỌ
-        Địa chỉ: 3/9A Đường số 6, F.9, Q.Gò Vấp, TP.HCM Điện thoại: 0908.433530 Website: alonhatro.com - Email: hotro@alonhatro.com
+        Địa chỉ: 3/9A Đường số 6, F.9, Q.Gò Vấp, TP.HCM
+          Điện thoại: 0908.433530 Website: alonhatro.com 
+          Email: hotro@alonhatro.com
       </footer>
     </>
   )
