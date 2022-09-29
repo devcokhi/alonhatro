2ae78d11c22e1348e3fc45c6d837f4dcfc19736b
devcokhi <69025946+devcokhi@users.noreply.github.com>
2022-09-29 15:18:21 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,56 +10,53 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+        <div class="container">
       <main>
-        <Header title="Kênh thông tin cho thuê bất động sản nhà trọ, phòng trọ hàng đầu Việt Nam" />
-  
-      <blockquote>
-<p>Kinh doanh <strong>cho thuê bất động sản</strong> là sự lựa chọn tốt để tạo ra dòng tiền thông minh. Từ số vốn nhỏ, thậm chí bằng 0, các nhà đầu tư hoàn toàn có thể có một nguồn lợi đều đặn và vô cùng lớn.</p>
-</blockquote>
- 
-<h2>Bất động sản cho thuê là gì?</h2>
-<p>Bất động sản cho thuê là mô hình kinh doanh thuê - thuê lại. Nghĩa là chủ đầu tư sẽ đi thuê nhà, căn hộ, chung cư, biệt thự,...tái tạo lại để cho người khác thuê và thu lợi nhuận từ khoản chênh.</p>
-<h2>Địa điểm tập trung bất động sản cho thuê</h2>
-<p>Thị trường cho thuê bất động sản là một thị trường sôi động, đầy tính cạnh tranh và chưa bao giờ có dấu hiệu hạ nhiệt. Để có thể hướng đến chính xác tệp khách hàng của mình, chủ đầu tư cần tìm đến những nơi tập trung nhiều mặt hàng này. Đó là những khu đô thị lớn như Hà Nội, TP.HCM, Đà Nẵng; các khu công nghiệp lớn, các khu du lịch, những nơi có mật độ dân số cao…Ở đây đều là những địa điểm thu hút người dân đến học tập, sinh sống và làm việc. Từ đó nhu cầu thuê nhà cũng tăng cao.</p>
-<h2>Lợi ích của cho thuê bất động sản</h2>
-<h3>Không cần bỏ nhiều vốn</h3>
-<p>Nếu các hình thức kinh doanh khác đòi hỏi bạn cần bỏ tiền thuê mặt bằng, đầu tư tài sản cố định, tiền trang thiết bị thì kinh doanh cho thuê bất động sản lại không cần bỏ ra những chi phí đó. Với phân khúc giá bất động sản đa dạng, chủ đầu tư có thể chủ động lựa chọn phân khúc đầu tư phù hợp với nguồn tài chính của mình.</p>
-<h3>Thu lợi đều đặn</h3>
-<p>Sử dụng bất động sản cho thuê sẽ giúp bạn có một nguồn thu đều đặn hàng tháng. Nếu bạn có số lượng cho thuê bất động sản lớn thì việc thu lợi còn khả thi hơn rất nhiều.</p>
- 
-<h3>Tính ổn định cao</h3>
-<p>Khách hàng đi thuê nhà ở hay văn phòng làm việc đều hướng tới sự ổn định. Vì vậy một khi đã ưng ý căn nhà, họ sẽ thường thuê lâu dài. Nếu chủ nhà có nhã ý như giảm giá nếu thuê lâu thì càng có thể giữ cân được khách hàng lâu hơn. Hiện nay nhu cầu nhà ở tăng cao do việc bỏ ra số tiền lớn để mua nhà là bất khả thi. Nhu cầu tăng đồng nghĩa với việc rủi ro mà bạn phải chịu cũng sẽ giảm đi.</p>
-<h3>Nhu cầu bất động sản lớn</h3>
-<p>Hiện nay có đến 80% người dân có nhu cầu mua nhà nhưng lại không có khả năng về tài chính.. Bên cạnh đó việc đổ dồn về các thành phố hơn học tập, làm việc và định cư vẫn được ưa chuộng, do đó lượng khách hàng có nhu cầu thuê nhà càng tăng lên.</p>
-<p>Ngoài ra, sự mở rộng và phát triển của các doanh nghiệp nước ngoài khiến cho phân khúc khách hàng quốc tế cũng tăng lên. Với những đối tượng này, chi phí thuê nhà cũng được đẩy giá lên. Ngoài ra, nhu cầu du lịch cũng là điều khiến nhiều người muốn thuê nhà trong khoảng thời gian ngắn hạn như 3 ngày - 1 tuần.</p>
-<h2>Những lưu ý khi kinh doanh cho thuê bất động sản</h2>
-<h3>Xem xét vị trí</h3>
-<p>Vị trí địa lý ảnh hưởng đến lợi nhuận chủ đầu tư có được từ việc cho thuê bất động sản. Với những phân khúc khách hàng khác nhau, số tiền họ có thể bỏ ra để thuê nhà cũng khác nhau. Do đó, bạn cần định hướng xem nhu cầu khách hàng nơi đó là gì để đầu tư cho hợp lý. Ví dụ với những khu công nghiệp, nhu cầu thuê trọ của công nhân là ngon- bổ- rẻ; với những thành phố lớn, người thuê có thu nhập cao, họ đòi hỏi một căn hộ cao cấp có đầy đủ tiện ích với chi phí đắt đỏ.</p>
-<h3>Tận dụng nguồn tài chính ngoài trong đầu tư</h3>
-<p>Đòn bẩy tài chính là yếu tố không thể thiếu khi đầu tư kinh doanh, đặc biệt là lĩnh vực bất động sản. Điều này nghĩa là nhà đầu tư huy động thêm vốn từ ngân hàng hoặc người thân quen với tư cách người cùng đầu tư. Một nhà đầu tư thông minh sẽ không bao giờ bỏ toàn toàn 100% vốn của mình để đầu tư. Việc này giống như “bỏ trứng vào một giỏ” sẽ dễ gây rủi ro khi kinh doanh. Thông thường họ chỉ dành số tiền đầu tư chiếm khoảng dưới 30% tổng giá trị bất động sản.</p>
-<h3>Đầu tư cho thuê là phương pháp tốt nhất và an toàn nhất</h3>
-<p>Mô hình cho thuê căn hộ, chung cư mini, nhà sẽ có giá trị thấp hơn mô hình cho thuê khu nghỉ dưỡng, thương&nbsp; mại, công nghiệp,...Tuy nhiên, loại hình đầu tư nhỏ này sẽ thu lời ổn định và an toàn hơn dành cho những người mới khởi nghiệp.</p>
-<p>Ví dụ trong thời gian dịch bệnh Covi-19 diễn ra, ngành du lịch, khách sạn bị cấm hoạt động, bất động sản nghỉ dưỡng bị ảnh hưởng nặng nề. Ngay cả bây giờ khi tình hình đã khả quan hơn nhưng người dân vẫn chưa sẵn sàng bỏ ra số tiền lớn để du lịch.</p>
- 
-<h3>Kích thước và độ phức tạp</h3>
-<p>Kích thước và độ phức tạp của bất động sản cho thuê sẽ ảnh hưởng đến lợi nhuận thu lại. Khu nghỉ dưỡng, tòa nhà lớn, khu công nghiệp đòi số vốn lớn và chấp nhận những áp lực cao. Thế nhưng đổi lại lợi nhuận có được lại vô cùng cao. Thông thường với giá trị thuê cao thì tính ổn định cũng lớn bởi người đi thuê thường khách hàng dài dạn, nộp tiền theo 5 năm thay vì hàng tháng như nhà trọ.</p>
-<h3>Hãy cho thuê bất động sản khi đã am hiểu lĩnh vực</h3>
-<p>Trước khi bắt tay vào lĩnh vực nào, bạn cần phải tìm hiểu kỹ càng về mảng đó. Đặc biệt với ngành dễ gặp rủi ro như kinh doanh cho thuê bất động sản. Hãy trang bị cho bản thân một kiến thức chuyên sâu, một chiến lược an toàn trước khi bỏ tiền vào đầu tư. Bạn có thể tham gia các khóa học kinh doanh hoặc học hỏi thực tế từ những đối thủ chuyên nghiệp hoặc bạn bè. Sau đó tham khảo các phần mềm quản lý bất động sản cho thuê để có thể mở rộng kinh doanh và thu lời cao.</p>
-<h2>Alo Nhà Trọ - Kênh thông tin cho thuê bất động sản uy tín</h2>
-<p>Để có thể thu được lợi nhuận từ việc <strong>cho thuê bất động sản</strong>, chắc chắn không thể thiếu được khách hàng. Vậy một câu hỏi là tìm nguồn khách hàng ở đâu? Câu trả lời chính là Alo Nhà Trọ. Đây là kênh thông tin cho thuê bất động kết nối người cho thuê và người cần thuê. Khách hàng có thể đăng tải thông tin miễn phí trên trang web. Chỉ cần đầy đủ thông tin hình ảnh, đợi admin phê duyệt là sản phẩm của bạn sẽ được lên kệ. Với hơn 1 triệu lượng truy cập mỗi tháng, bạn có thể tiếp cận được với khách hàng mục tiêu với số lượng lớn. Về phía khách cho nhu cầu thuê, với hơn 20 nghìn bài đăng mỗi tháng, bạn có thể đa dạng sự lựa chọn.</p>
-<p>Qua bài viết trên, hy vọng bạn đã trang bị được phần nào chút kinh nghiệm để đầu tư cho thuê bất động sản. <a href="https://alonhatro.com/">Alo Nhà Trọ</a> sẽ cùng đồng hành hỗ trợ các nhà đầu tư để kết nối với khách hàng tiềm năng.</p> 
-     <div class="col-lg-6 font15">
-				<div>Thời gian làm việc</div>
-<p>Thứ Hai - Thứ Sáu: 08:00–19:00</p>
-<p>Thứ Bảy: 08:00–19:00</p>
-<p>Chủ Nhật: Đóng cửa</p>
-<div><label>Địa chỉ: 3/9A Đường số 6, F.9, Q.Gò Vấp, TP.HCM</label></div>
-<div><label>Điện thoại:&nbsp;0908.433.530</label></div>
-<div><label>Website: <a href="https://alonhatro.com" rel="nofollow">alonhatro.com</a>&nbsp;</label></div>
-<div><label>Email: <a href="mailto:hotro@alonhatro.com" rel="nofollow">hotro@alonhatro.com</a></label></div>
-<div ><iframe width="300" height="150" style="border: 0;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.5455774067195!2d106.65360331532503!3d10.846045992274345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3175296f2d27de79%3A0x52926335c53c78b5!2zQWxvIE5ow6AgVHLhu40!5e0!3m2!1svi!2s!4v1577581063366!5m2!1svi!2s" frameborder="0" allowfullscreen="allowfullscreen"></iframe></div>
-      </div>  
+        <h1 class="title">Kênh thông tin cho thuê bất động sản nhà trọ, phòng trọ hàng đầu Việt Nam - Alo Nhà trọ</h1>
+        <blockquote><p>Kinh doanh <strong>cho thuê bất động sản</strong> là sự lựa chọn tốt để tạo ra dòng tiền thông minh. Từ số vốn nhỏ, thậm chí bằng 0, các nhà đầu tư hoàn toàn có thể có một nguồn lợi đều đặn và vô cùng lớn. Cùng <a href="https://alonhatro.com/">Alo Nhà Trọ</a> tìm hiều bất động  sản cho thuê </p></blockquote>
+        <figure >
+          <img src="https://alonhatro.com/assets/data/res/alonhatro-website.png" alt="Kênh thông tin cho thuê bất động sản nhà trọ, phòng trọ hàng đầu Việt Nam - Alo Nhà trọ" >
+        </figure>
+        <h2>Bất động sản cho thuê là gì?</h2>
+        <p>Bất động sản cho thuê là mô hình kinh doanh thuê - thuê lại. Nghĩa là chủ đầu tư sẽ đi thuê nhà, căn hộ, chung cư, biệt thự,...tái tạo lại để cho người khác thuê và thu lợi nhuận từ khoản chênh.</p>
+        <h2>Địa điểm tập trung bất động sản cho thuê</h2>
+        <p>Thị trường cho thuê bất động sản là một thị trường sôi động, đầy tính cạnh tranh và chưa bao giờ có dấu hiệu hạ nhiệt. Để có thể hướng đến chính xác tệp khách hàng của mình, chủ đầu tư cần tìm đến những nơi tập trung nhiều mặt hàng này. Đó là những khu đô thị lớn như Hà Nội, TP.HCM, Đà Nẵng; các khu công nghiệp lớn, các khu du lịch, những nơi có mật độ dân số cao…Ở đây đều là những địa điểm thu hút người dân đến học tập, sinh sống và làm việc. Từ đó nhu cầu thuê nhà cũng tăng cao.</p>
+        <h2>Lợi ích của cho thuê bất động sản</h2>
+        <h3>Không cần bỏ nhiều vốn</h3>
+        <p>Nếu các hình thức kinh doanh khác đòi hỏi bạn cần bỏ tiền thuê mặt bằng, đầu tư tài sản cố định, tiền trang thiết bị thì kinh doanh cho thuê bất động sản lại không cần bỏ ra những chi phí đó. Với phân khúc giá bất động sản đa dạng, chủ đầu tư có thể chủ động lựa chọn phân khúc đầu tư phù hợp với nguồn tài chính của mình.</p>
+        <h3>Thu lợi đều đặn</h3>
+        <p>Sử dụng bất động sản cho thuê sẽ giúp bạn có một nguồn thu đều đặn hàng tháng. Nếu bạn có số lượng cho thuê bất động sản lớn thì việc thu lợi còn khả thi hơn rất nhiều.</p>
+        <h3>Tính ổn định cao</h3>
+        <p>Khách hàng đi thuê nhà ở hay văn phòng làm việc đều hướng tới sự ổn định. Vì vậy một khi đã ưng ý căn nhà, họ sẽ thường thuê lâu dài. Nếu chủ nhà có nhã ý như giảm giá nếu thuê lâu thì càng có thể giữ cân được khách hàng lâu hơn. Hiện nay nhu cầu nhà ở tăng cao do việc bỏ ra số tiền lớn để mua nhà là bất khả thi. Nhu cầu tăng đồng nghĩa với việc rủi ro mà bạn phải chịu cũng sẽ giảm đi.</p>
+        <h3>Nhu cầu bất động sản lớn</h3>
+        <p>Hiện nay có đến 80% người dân có nhu cầu mua nhà nhưng lại không có khả năng về tài chính.. Bên cạnh đó việc đổ dồn về các thành phố hơn học tập, làm việc và định cư vẫn được ưa chuộng, do đó lượng khách hàng có nhu cầu thuê nhà càng tăng lên.</p>
+        <p>Ngoài ra, sự mở rộng và phát triển của các doanh nghiệp nước ngoài khiến cho phân khúc khách hàng quốc tế cũng tăng lên. Với những đối tượng này, chi phí thuê nhà cũng được đẩy giá lên. Ngoài ra, nhu cầu du lịch cũng là điều khiến nhiều người muốn thuê nhà trong khoảng thời gian ngắn hạn như 3 ngày - 1 tuần.</p>
+        <h2>Những lưu ý khi kinh doanh cho thuê bất động sản</h2>
+        <h3>Xem xét vị trí</h3>
+        <p>Vị trí địa lý ảnh hưởng đến lợi nhuận chủ đầu tư có được từ việc cho thuê bất động sản. Với những phân khúc khách hàng khác nhau, số tiền họ có thể bỏ ra để thuê nhà cũng khác nhau. Do đó, bạn cần định hướng xem nhu cầu khách hàng nơi đó là gì để đầu tư cho hợp lý. Ví dụ với những khu công nghiệp, nhu cầu thuê trọ của công nhân là ngon- bổ- rẻ; với những thành phố lớn, người thuê có thu nhập cao, họ đòi hỏi một căn hộ cao cấp có đầy đủ tiện ích với chi phí đắt đỏ.</p><h3>Tận dụng nguồn tài chính ngoài trong đầu tư</h3><p>Đòn bẩy tài chính là yếu tố không thể thiếu khi đầu tư kinh doanh, đặc biệt là lĩnh vực bất động sản. Điều này nghĩa là nhà đầu tư huy động thêm vốn từ ngân hàng hoặc người thân quen với tư cách người cùng đầu tư. Một nhà đầu tư thông minh sẽ không bao giờ bỏ toàn toàn 100% vốn của mình để đầu tư. Việc này giống như “bỏ trứng vào một giỏ” sẽ dễ gây rủi ro khi kinh doanh. Thông thường họ chỉ dành số tiền đầu tư chiếm khoảng dưới 30% tổng giá trị bất động sản.</p>
+        <h3>Đầu tư cho thuê là phương pháp tốt nhất và an toàn nhất</h3>
+        <p>Mô hình cho thuê căn hộ, chung cư mini, nhà sẽ có giá trị thấp hơn mô hình cho thuê khu nghỉ dưỡng, thương  mại, công nghiệp,...Tuy nhiên, loại hình đầu tư nhỏ này sẽ thu lời ổn định và an toàn hơn dành cho những người mới khởi nghiệp.</p>
+        <p>Ví dụ trong thời gian dịch bệnh Covi-19 diễn ra, ngành du lịch, khách sạn bị cấm hoạt động, bất động sản nghỉ dưỡng bị ảnh hưởng nặng nề. Ngay cả bây giờ khi tình hình đã khả quan hơn nhưng người dân vẫn chưa sẵn sàng bỏ ra số tiền lớn để du lịch.</p>
+        <h3>Kích thước và độ phức tạp</h3>
+        <p>Kích thước và độ phức tạp của bất động sản cho thuê sẽ ảnh hưởng đến lợi nhuận thu lại. Khu nghỉ dưỡng, tòa nhà lớn, khu công nghiệp đòi số vốn lớn và chấp nhận những áp lực cao. Thế nhưng đổi lại lợi nhuận có được lại vô cùng cao. Thông thường với giá trị thuê cao thì tính ổn định cũng lớn bởi người đi thuê thường khách hàng dài dạn, nộp tiền theo 5 năm thay vì hàng tháng như nhà trọ.</p><h3>Hãy cho thuê bất động sản khi đã am hiểu lĩnh vực</h3>
+        <p>Trước khi bắt tay vào lĩnh vực nào, bạn cần phải tìm hiểu kỹ càng về mảng đó. Đặc biệt với ngành dễ gặp rủi ro như kinh doanh cho thuê bất động sản. Hãy trang bị cho bản thân một kiến thức chuyên sâu, một chiến lược an toàn trước khi bỏ tiền vào đầu tư. Bạn có thể tham gia các khóa học kinh doanh hoặc học hỏi thực tế từ những đối thủ chuyên nghiệp hoặc bạn bè. Sau đó tham khảo các phần mềm quản lý bất động sản cho thuê để có thể mở rộng kinh doanh và thu lời cao.</p>
+        <h2>Alo Nhà Trọ - Kênh thông tin cho thuê bất động sản uy tín</h2>
+        <p>Để có thể thu được lợi nhuận từ việc <strong>cho thuê bất động sản</strong>, chắc chắn không thể thiếu được khách hàng. Vậy một câu hỏi là tìm nguồn khách hàng ở đâu? Câu trả lời chính là Alo Nhà Trọ. Đây là kênh thông tin cho thuê bất động kết nối người cho thuê và người cần thuê. Khách hàng có thể đăng tải thông tin miễn phí trên trang web. Chỉ cần đầy đủ thông tin hình ảnh, đợi admin phê duyệt là sản phẩm của bạn sẽ được lên kệ. Với hơn 1 triệu lượng truy cập mỗi tháng, bạn có thể tiếp cận được với khách hàng mục tiêu với số lượng lớn. Về phía khách cho nhu cầu thuê, với hơn 20 nghìn bài đăng mỗi tháng, bạn có thể đa dạng sự lựa chọn.</p>
+        <p>Qua bài viết trên, hy vọng bạn đã trang bị được phần nào chút kinh nghiệm để đầu tư cho thuê bất động sản. <a href="https://alonhatro.com/">Alo Nhà Trọ</a> sẽ cùng đồng hành hỗ trợ các nhà đầu tư để kết nối với khách hàng tiềm năng.</p>
+        <blockquote>
+          <p>Nói về Alo Nhà Trọ</p>
+          <ul>
+            <li><a href="https://dev.azure.com/nguyenhoa2905/Alonhatro/_wiki/wikis/Alonhatro.wiki/1/Alo-Nha-Tro-Kenh-thong-tin-cho-thue-bat-%C4%91ong-san-uy-tin"> https://dev.azure.com/nguyenhoa2905/Alonhatro/_wiki/wikis/Alonhatro.wiki/1/Alo-Nha-Tro-Kenh-thong-tin-cho-thue-bat-%C4%91ong-san-uy-tin</a></li>
+            <li><a href="https://bitbucket.org/ahabook/alonhatro.com/src/master/README.md">https://bitbucket.org/ahabook/alonhatro.com/src/master/README.md</a></li>
+            <li><a href="https://storage.googleapis.com/alonhatro/aboutalonhatro.html">https://storage.googleapis.com/alonhatro/aboutalonhatro.html</a></li>
+            <li><a href="https://alonhatro.netlify.app/"> https://alonhatro.netlify.app/</a></li>
+            <li><a href="https://blogalonhatro.s3.ap-southeast-1.amazonaws.com/aboutalonhatro.html">https://blogalonhatro.s3.ap-southeast-1.amazonaws.com/aboutalonhatro.html</a></li>
+            <li><a href="https://alonhatro.s3.us-west-004.backblazeb2.com/aboutalonhatro.html">https://alonhatro.s3.us-west-004.backblazeb2.com/aboutalonhatro.html</a></li>
+          </ul>
+        </blockquote>
       </main>
+      <footer>ALO NHÀ TRỌ Địa chỉ: 3/9A Đường số 6, F.9, Q.Gò Vấp, TP.HCM Điện thoại: 0908.433530 Website: alonhatro.com Email: hotro@alonhatro.com</footer>
+    </div>
 
       <Footer />
     </div>
